7a07cd52eace30a510f6925ab5811419c65ce692
Tauqeer Ahmad <tauqeer.ahmad2008@gmail.com>
2023-04-03 16:52:46 +0500

    Board: Added board validations

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,13 @@
 class Board < ApplicationRecord
   belongs_to :user
+
+  validates :name, :width, :height, :mines, presence: true
+  validates :width, :height, :mines, numericality: { only_integer: true, greater_than: 0 }
+  validate :mines_less_than_total_cells
+
+  def mines_less_than_total_cells
+    if mines.present? && width.present? && height.present? && mines >= width * height
+      errors.add(:mines, "must be less than total cells")
+    end
+  end
 end
